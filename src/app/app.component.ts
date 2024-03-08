bdd7ec4468692950d52b8e43feaa0208a67797a4
aleverifc <153004563+aleverifc@users.noreply.github.com>
2024-03-07 19:42:33 -0500

    Intial structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { GeneratorService } from './services/generator.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  regex: string = '';
+  text: string = '';
+  result: string = '';
+  isValid: boolean = false;
+  allowNum: boolean = false;
+  allowAlph: boolean = false;
+  min: number = 0;
+  max: number = 0;
+  specialChars: string = '';
+  notSpecialChars: string = '';
+
+  constructor(private generatorService: GeneratorService) {
+  }
+
+  validate() {
+    if (!this.isNullOrEmpty(this.result)) {
+      this.generatorService.getData(this.text, this.result).subscribe((x: any) => {
+        this.isValid = x.isValid;
+      });
+    }
+  }
+
+  generate() {
+    let configObject = {
+      allowNumeric: this.allowNum,
+      allowAlphaNumeric: this.allowAlph,
+      minLength: this.min,
+      maxLength: this.max,
+      allowedSpecialChars: this.specialChars,
+      notAllowedSpecialChars: this.notSpecialChars
+    };
+    this.generatorService.postData(configObject).subscribe((x: any) => {
+      this.result = x.expression;
+    });
+  }
+
+  isNullOrEmpty(value: string): boolean {
+    return !value || value.trim().length === 0;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
